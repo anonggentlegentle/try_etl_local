8e7850f8f33e2b71a7a30d72550c6795f0d10b69
metamusk <balberanjarenkim@gmail.com>
2025-03-20 19:10:49 +0800

    Adding the feature to select the files using file explorer rather than with textfile

--- etl_funcs.py
+++ etl_funcs.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import openpyxl
 from datetime import datetime
+from tkinter.filedialog import askopenfilename, askdirectory
+from excel_formatting import format_excel_file
 
 def extract_times(file_path, col_names):
     time_df = pd.read_csv(file_path, names=col_names, skiprows=1)
@@ -20,8 +22,12 @@ def transform_name(transform_df):
 
     return fullname_df
 
-def load_to_excel(dataframe, file_path):
-    dataframe.to_excel(file_path, engine="openpyxl", index=False)
+def load_to_excel(dataframe, final_file_path):
+    final_file = final_file_path.replace(".csv", "-NEW.xlsx")
+
+    dataframe.to_excel(final_file, engine="openpyxl", index=False)
+
+    format_excel_file(final_file)
 
 if __name__ == "__main__":
     print("Not to be used on it's own, functions must be imported")
--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from etl_funcs import extract_times, transform_name, load_to_excel
 from excel_formatting import format_excel_file
+import tkinter as tk
+from tkinter.filedialog import askopenfilename, askdirectory
 
 while True:
     try:
-        print("Enter the CSV filename in 'MM-DD-YYYY' format :")
-        file_name = input()
+        # print("Enter the CSV filename in 'MM-DD-YYYY' format :")
+        # file_name = input()
 
         time_cols = ["id", "First Name", "Last Name", "Biometrics", "Time", "Date"]
 
-        time_path = f"C:\\Users\\USER\\Desktop\\{file_name}.csv"
+        # time_path = f"C:\\Users\\USER\\Desktop\\{file_name}.csv"
 
-        new_time_path = f"C:\\Users\\USER\\Desktop\\{file_name}-NEW.xlsx"
+        # new_time_path = f"C:\\Users\\USER\\Desktop\\{file_name}-NEW.xlsx"
 
-        df_extract = extract_times(time_path, time_cols)
+        file_path = askopenfilename().replace("/", "\\")
+
+        final_file_path = file_path.replace("/", "\\")
+
+        df_extract = extract_times(file_path, time_cols)
 
         transformed_df = transform_name(df_extract)
 
-        load_to_excel(transformed_df, new_time_path)
-
-        format_excel_file(new_time_path)
+        load_to_excel(transformed_df, final_file_path)
     except FileNotFoundError:
         print(f"The CSV file named cannot be found. Please enter a filename again.")
         continue
